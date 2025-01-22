ac0d031ec351789f7d4ddf0b0c75aeaf8cb4d20e
OH-SEOKHOON <oshosh240@gmail.com>
2025-01-22 15:27:24 +0900

    리소스 프리팹들 추가

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -2,17 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+public class GameManager : Singleton<GameManager>
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject StartPanel;
+    public GameObject EndPanel;
+    public GameObject car;
+    public GameObject road;
+    public GameObject gas;
+
+    public GameObject[] roads = new GameObject[8];
+    public override void OnAwake()
     {
-        
+        base.OnAwake();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void StartGame()
     {
+        StartPanel.SetActive(false);
+        SpawnRoad();
+    }
+
+    public void SpawnRoad()
+    {
+        for (int i = 0; i < roads.Length; i++)
+        {
+            roads[i] = Instantiate(road, new Vector2(0, -4 + (i*2)), Quaternion.identity);
+        }
         
+        CheckArray();
+    }
+    
+    void CheckArray()
+    {
+        for (int i = 0; i < roads.Length; i++)
+        {
+            if (roads[i] == null)
+            {
+                Debug.LogError($"Road {i} is missing!");
+            }
+            else
+            {
+                Debug.Log($"Road {i} exists at position: {roads[i].transform.position}");
+            }
+        }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # recegame
+
+도로배경사진 출처: 아티스트 Mariusz Szulc님 작품
+https://www.behance.net/MariuszSzulc
